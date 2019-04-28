4c7dbe4fbc846d6b8709670db0d8f1c29474bd1c
caiosuzuki <cpolidoro@gmail.com>
2019-04-27 22:17:02 -0400

    adicionando codigo ao status da familia

--- src/main/java/com/dulcedomum/dominio/familia/StatusDaFamilia.java
+++ src/main/java/com/dulcedomum/dominio/familia/StatusDaFamilia.java
@@ -1,5 +1,18 @@
 package com.dulcedomum.dominio.familia;
 
 public enum StatusDaFamilia {
-    CADASTRO_VALIDO, JA_POSSUI_UMA_CASA, SELECIONADA_EM_OUTRO_PROCESSO_DE_SELECAO, CADASTRO_INCOMPLETO;
+    CADASTRO_VALIDO(0),
+    JA_POSSUI_UMA_CASA(1),
+    SELECIONADA_EM_OUTRO_PROCESSO_DE_SELECAO(2),
+    CADASTRO_INCOMPLETO(3);
+
+    private int codigo;
+
+    StatusDaFamilia (int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
 }
