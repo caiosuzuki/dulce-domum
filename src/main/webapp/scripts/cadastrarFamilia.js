5036c2e196b3cbc04e27e7247489f134227cc390
caiosuzuki <cpolidoro@gmail.com>
2019-07-16 21:28:43 -0400

    obtendo dados de cada pessoa da família em formato de objeto

--- src/main/webapp/scripts/cadastrarFamilia.js
+++ src/main/webapp/scripts/cadastrarFamilia.js
@@ -7,14 +7,12 @@ function cadastrarMaisUmaPessoaDaFamilia() {
 function enviarCadastroDeFamilia() {
     const formulariosDePessoas = Array.from(document.querySelectorAll('.formulario-de-pessoa-da-familia'));
     let listaDeDadosDasPessoasDaFamilia = formulariosDePessoas.map(formularioDaPessoa => obterJsonDosDadosDaPessoa(formularioDaPessoa));
-    console.log(listaDeDadosDasPessoasDaFamilia);
 }
 
 function obterJsonDosDadosDaPessoa(formularioDaPessoa) {
     const elementosDoFormularioDaPessoa = formularioDaPessoa.elements;
     return [].reduce.call(elementosDoFormularioDaPessoa, (dados, elemento) => {
-        dados[elemento.name] = elemento.value;
+        dados[elemento.dataset.js] = elemento.value;
         return dados;
     }, {});
-    // console.log(elementosDoFormularioDaPessoa);
 }
