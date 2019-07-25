2e1abc1e2d8999a26319b80c42e39f57e68607f8
caiosuzuki <cpolidoro@gmail.com>
2019-07-24 22:05:59 -0400

    consertando redirecionamento da antiga pagina inicial

--- src/main/frontend/scripts/antigaPaginaInicial.js
+++ src/main/frontend/scripts/antigaPaginaInicial.js
@@ -1,11 +1,12 @@
 function irParaNovaHome() {
-    let senhaDigitada = window.prompt("Digite a senha para acessar o produto: ");
+    let senhaDigitada = window.prompt('Digite a senha para acessar o produto: ');
     if (senhaDigitada == "senha123") {
-        let usuarioQuerAbrirEmNovaAba = window.confirm("Deseja abrir em nova aba?");
+        let usuarioQuerAbrirEmNovaAba = window.confirm('Deseja abrir em nova aba?');
         if (usuarioQuerAbrirEmNovaAba) {
-            window.open("http://127.0.0.1:61998/src/main/frontend/paginaInicial.html");
+            // window.open("http://127.0.0.1:61998/src/main/frontend/paginaInicial.html");
+            window.open('paginaInicial.html');
         } else {
-            window.location = "http://127.0.0.1:61998/src/main/frontend/paginaInicial.html";
+            window.location = 'paginaInicial.html';
         }
     } else {
         window.alert('Senha incorreta, tente novamente.')
